6ff3ba35f3c5e3ad09aeb841ef404b814eaf8822
Daniick56 <danielballesteros22@outlook.com>
2024-09-10 17:56:29 -0500

    saving changes

--- FRONT/src/pages/login/Login.jsx
+++ FRONT/src/pages/login/Login.jsx
@@ -2,6 +2,7 @@ import './login.css'
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../../logic/authPlayer';
 import { useForm } from 'react-hook-form'
+import { io } from 'socket.io-client';
 import { useEffect, useState } from 'react';
 
 export const Login = ()=> {
@@ -9,15 +10,36 @@ export const Login = ()=> {
     const navigate = useNavigate();
     const [code, setCode] = useState(null);
     const {playerState, play} = useAuth();
-    const [navigateState, setNavigateState] = useState(false);
+    const [socket, setSocket] = useState(null);
+
+    //creando la conexión del socket
+    useEffect(()=> {
+        const newSocket = io('http://localhost:3000');
+        setSocket(newSocket);
+
+        return ()=>newSocket.close();
+    }, []);
+    
+    //escuchando los eventos:
+    useEffect(()=>{
+        if (socket) {
+            socket.on('game:code', ((codigo)=>{
+                console.log(codigo);
+                console.log('ola');
+                
+            }));
+
+        }
+
+    }, [socket])
 
     const handleOnClick = ()=> {
         
         play();
         play();
-        let code = generateCode();
         setCode(code);
-        setNavigateState(true);
+        socket.emit('game:newGame', 'testing');
+
     }
 
 
@@ -32,12 +54,12 @@ export const Login = ()=> {
         <div className='form--container container min-vh-100'>
             <div id='card--login' className="card shadow-lg p-4">
                 <div id='card--body--login' className="card--body--login card-body display-flex justify-content-center">
-                    <form onSubmit={handleSubmit(onSubmit)} className='form display-flex justify-content-center'>
                         <h1 id='title--login' className='form-title'>Siigo Match Battle <br />⚔️</h1>
                         <div className="mb-5 mt-5">
                             <p className='fs-3 mt-3 mb-3'>Create a game:</p>
                             <button onClick={handleOnClick} className='btn btn-primary'>Create</button>
                         </div>
+                    <form onSubmit={handleSubmit(onSubmit)} className='form display-flex justify-content-center'>
                         <div className="mt-5">
                             <label htmlFor="match" className="form-label fs-3">Join a game:</label><br />
                             <input {...register('match', {
