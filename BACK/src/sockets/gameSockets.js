5c972e74c69fc3e7fdaf858067d908906f10f45f
Samuel Martinez <samuelmartinezruiz43@gmail.com>
2024-09-12 17:04:37 -0500

    Back v2.1

--- BACK/src/sockets/gameSockets.js
+++ BACK/src/sockets/gameSockets.js
@@ -3,6 +3,7 @@ import { newRoom, getRoomByHex } from "../repositories/roomsRepo.js"
 import { cards } from "../cards/cards.js"
 import { shuffler } from "../utils/shuffler.js"
 import { getRoomSize } from "../utils/getRoomSize.js"
+import { setFirstPlayer } from "../utils/setFirstPlayer.js"
 
 export function newGame(io, socket) {
     socket.on('game:newGame', async (data)=> {
@@ -14,11 +15,11 @@ export function newGame(io, socket) {
             if(!code){
                 return socket.emit('game:error', {message: 'Error in the hexGenerator'})
             }
-            socket.join(code)
+            await socket.join(code)
             await newRoom(code)
             socket.emit('game:code', code) // envio codigo hex y creo la sala
         }catch(err){
-            socket.emit('game:error', {message: 'Error creating the game', error: err.message})
+            socket.emit('game:error', {message: 'INTERNAL SERVER ERROR', error: err.message})
         }
     })
 }
@@ -36,25 +37,59 @@ export function joinRoom(io, socket){
             socket.emit('game:joinRoom', data)
             io.to(data).emit("game:newPlayer", playersN) // numero de jugadores
         } catch(err){
-            socket.emit('game:error', {message: 'Error joining the game', error: err.message})
+            socket.emit('game:error', {message: 'INTERNAL SERVER ERROR', error: err.message})
         }
     })
 }
 
+
+let firstCards = {}
 export function startGame(io, socket){
-    socket.on('game:startGame', async (data) =>{ // data = "codigo hex de la sala"
+    socket.on('game:startGame', (data) =>{ // data = "codigo hex de la sala"
         try{
             const socketsInRoom = io.sockets.adapter.rooms.get(data)
             const playersN = getRoomSize(data)
             const shuffledCards = shuffler(cards, playersN)
             let i = 0
+
+            firstCards[data] = {
+                cards: []
+            }
+
             socketsInRoom.forEach(socketId => {
                 io.to(socketId).emit('game:cards', {cards: shuffledCards[i], size: playersN})// cards: un array de arrays, cada uno con 4 cartas aleatorias, size: numero de jugadores en la room
                 i++
             })
             i = 0
         } catch(err){
-            socket.emit('game:error', {message: 'Error starting the game', error: err.message})
+            socket.emit('game:error', {message: 'INTERNAL SERVER ERROR', error: err.message})
+        }
+    })
+}
+
+/* 
+4  Start round
+    => socket.emit(“game:startRound”, data )  data = { code: hex, card: primera carta de la baraja } 
+    <= socket.on(“game:selectPlayer”, data ) data = “id carta inicial” 
+*/
+
+
+
+export function startRound(io, socket){
+    socket.on('game:startRound', (data) => { // data = { code: hex, card: primera carta de la baraja } 
+        try{
+            let playersN = getRoomSize(data.code)
+            if(!firstCards[data.code]){
+                return socket.emit('game:error', { message: 'Room not found' })
+            }
+            if(playersN == firstCards[data.code].cards.length){
+                let firstCardId = setFirstPlayer(firstCards[data.code].cards)
+                socket.emit("game:selectPlayer", firstCardId )
+            }else{
+                firstCards[data.code].cards.push(data.card)
+            }
+        }catch(err){
+            socket.emit('game:error', {message: 'INTERNAL SERVER ERROR', error: err.message})
         }
     })
 }
