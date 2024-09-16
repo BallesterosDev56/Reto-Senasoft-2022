2228f69a4d68d2f6d5bed067d3d748a868dadd69
ratprogramer <lsamuelmartinezruiz43@gmail.com>
2024-09-15 20:01:00 -0500

    v4.0

--- BACK/src/utils/setFirstPlayer.js
+++ BACK/src/utils/setFirstPlayer.js
@@ -4,35 +4,46 @@ export function setFirstPlayer (array){
         array.forEach(card => {
             letras.push(card.id)
         })
-        let max = ""
+        let max = []
         let ordenadas = letras.sort()
+        console.log(ordenadas);
+        
         ordenadas.forEach(letra => {
-            if(letra[1] == "4"){
-                max = letra
-                return
-            }
-        })
-        ordenadas.forEach(letra => {
-            if(letra[1] == "3"){
-                max = letra
+            if(letra[1] == "1"){
+                max.push(letra)
                 return
             }
         })
         ordenadas.forEach(letra => {
             if(letra[1] == "2"){
-                max = letra
+                max.push(letra)
                 return
             }
         })
         ordenadas.forEach(letra => {
-            if(letra[1] == "1"){
-                max = letra
+            if(letra[1] == "3"){
+                max.push(letra)
                 return
             }
         })
-        return max
+        ordenadas.forEach(letra => {
+            if(letra[1] == "4"){
+                max.push(letra)
+                return
+            }
+        })
+        return max[0]
     }catch (err){
         console.log(err)
     }
 }
 
+export const cards = [
+    { id: "F3", photo: "https://storage.googleapis.com/url-images-generator.appspot.com/1726157076239_Giulia.png", modelo: "Giulia", cilindraje: 2900, potencia: 505, topSpeed: 307, peso: 1500 },
+    { id: "H3", photo: "https://storage.googleapis.com/url-images-generator.appspot.com/1726158376874_Grand Cherokee.png", modelo: "Grand Cherokee", cilindraje: 3600, potencia: 293, topSpeed: 210, peso: 2000 },
+    { id: "I3", photo: "https://storage.googleapis.com/url-images-generator.appspot.com/1726158376874_Grand Cherokee.png", modelo: "Grand Cherokee", cilindraje: 3600, potencia: 293, topSpeed: 210, peso: 2000 },
+
+]
+
+let a = setFirstPlayer(cards)
+console.log(a);
